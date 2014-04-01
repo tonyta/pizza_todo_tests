2a249acb8262925567677b4a602fa6972dc52ecc
Tony Ta <tonyta.tt@gmail.com>
2014-04-01 10:56:58 -0500

    markascomplete test

--- todo-list/todo_spec.rb
+++ todo-list/todo_spec.rb
@@ -22,17 +22,26 @@ describe Todo  do
     todo.should respond_to(:created_at)
   end
 
-  it "should expect a DateTime for the timestamp" do
+
+  it "expects a DateTime for the timestamp" do
     todo.created_at.class.should be == DateTime
   end
 
 
-  it "should have a default status of 'incomplete'" do
+  it "has a default status of 'incomplete'" do
     todo.status.should be =="incomplete"
   end
 
-  it "should be initialized with a created_at timetamp" do
+  it "is initialized with a created_at timetamp" do
     todo.created_at.should_not be_nil
   end
 
+  it "has a 'mark_as_complete!' method" do
+    todo.should respond_to(:mark_as_complete!)
+  end
+
+  it "sets its status to 'complete' when mark_as_complete! is called" do
+    todo.mark_as_complete!
+    todo.status.should == "complete"
+  end
 end
